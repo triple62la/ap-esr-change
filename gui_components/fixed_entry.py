1c14e6229f1eba2d68b747e1c09bd1741bc6bd95
rakhimzhon.dovidov <rkdovidov@rt-dc.ru>
2023-12-19 18:42:59 +0300

    fix: fix bug with copy/paste selected

--- gui_components/fixed_entry.py
+++ gui_components/fixed_entry.py
@@ -11,9 +11,13 @@ class FixedEntry(tk.Entry):
 
     def _keypress(self, e: tk.Event):
         if e.keycode == 86 and e.keysym != 'v':
+            if self.selection_present():
+                self.delete(tk.SEL_FIRST,tk.SEL_LAST)
             self.insert(tk.INSERT,clipboard.paste())
         elif e.keycode == 67 and e.keysym != 'c':
-            clipboard.copy(self.selection_get())
+            if self.selection_present():
+                clipboard.copy(self.selection_get())
         elif e.keycode == 88 and e.keysym != 'x':
-            clipboard.copy(self.selection_get())
-            self.delete(tk.SEL_FIRST,tk.SEL_LAST)
+            if self.selection_present():
+                clipboard.copy(self.selection_get())
+                self.delete(tk.SEL_FIRST,tk.SEL_LAST)
